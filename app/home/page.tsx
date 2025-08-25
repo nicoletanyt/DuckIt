9b9c3e342d27ecead66585ddd3bed5ffa16985fe
Sean Chua <88256324+SeansC12@users.noreply.github.com>
2025-08-25 23:40:24 +0800

    Set up project repo with Supabase auth

---
+++ app/home/page.tsx
@@ -0,0 +1,20 @@
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/lib/supabase/server";
+
+export default async function ProtectedPage() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getClaims();
+  if (error || !data?.claims) {
+    redirect("/auth/login");
+  }
+
+  return (
+    <div className="flex-1 w-full flex flex-col gap-12">
+      <div className="w-full">
+          Protected page.
+      </div>
+    </div>
+  );
+}
