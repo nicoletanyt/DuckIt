6a93d90d51822c05caa733c436e6f6ce27655ea4
Sean Chua <88256324+SeansC12@users.noreply.github.com>
2025-08-26 22:11:45 +0800

    Remove footer in unprotected home page

--- app/page.tsx
+++ app/page.tsx
@@ -19,9 +19,9 @@ export default function Home() {
           </main>
         </div>
 
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+        {/* <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
           <ThemeSwitcher />
-        </footer>
+        </footer> */}
       </div>
     </main>
   );
