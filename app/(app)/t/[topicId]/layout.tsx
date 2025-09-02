f5ee59a40fd619767a3c82d60bef9753651a1533
Sean Chua <88256324+SeansC12@users.noreply.github.com>
2025-09-02 22:40:42 +0800

    Add layout.tsx file for both the topic page and session summary page. Temporarily remove the recording functionality.

---
+++ app/(app)/t/[topicId]/layout.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { use, useEffect, useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { createClient } from "@/lib/supabase/client";
+
+export default function TopicLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: Promise<{ topicId: string }>;
+}) {
+  const [topicTitle, setTopicTitle] = useState("Loading...");
+  const { topicId } = use(params);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const fetchTopicTitle = async () => {
+      const supabase = createClient();
+      const { data: topicName } = await supabase.from("topics").select("topic_title").eq("id", topicId).single();
+      setTopicTitle(topicName?.topic_title || "Error retrieving topic.");
+    };
+
+    fetchTopicTitle();
+  }, [topicId]);
+
+
+  const isSessionsPage = pathname?.includes('/sessions');
+  const defaultTab = isSessionsPage ? "summary" : "session";
+
+  return (
+    <div className="px-20 py-10 space-y-8">
+      <h1>{topicTitle}</h1>
+      <Tabs value={defaultTab}>
+        <TabsList>
+          <Link href={`/t/${topicId}`}>
+            <TabsTrigger
+              value="session"
+              className="dark:data-[state=active]:text-white"
+            >
+              New Session
+            </TabsTrigger>
+          </Link>
+          <Link href={`/t/${topicId}/sessions`}>
+            <TabsTrigger
+              value="summary"
+              className="dark:data-[state=active]:text-white"
+            >
+              Summary
+            </TabsTrigger>
+          </Link>
+        </TabsList>
+        <TabsContent value={defaultTab}>
+          {children}
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
--- app/(app)/t/[topicId]/page.tsx
+++ app/(app)/t/[topicId]/page.tsx
@@ -1,18 +1,7 @@
 "use client";
 
-import { Suspense, use, useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
-import "@/app/globals.css";
-
-// import interfaces
-import { Session } from "@/lib/Session";
-import { ViewType } from "@/lib/ViewType";
-import { Summary } from "@/lib/Summary";
-
-// import components
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Suspense, use, useState } from "react";
 import { Button } from "@/components/ui/button";
-import SearchBar from "@/components/search-bar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -20,63 +9,10 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { buttonVariants } from "@/components/ui/button";
-import SessionCard from "@/components/session-card";
 import FileItem from "@/components/file-item";
-import SummaryDetails from "@/components/summary-detail";
 import Link from "next/link";
-
-// Import icons
-import {
-  ArrowLeft,
-  Import,
-  LayoutGrid,
-  LayoutList,
-  Mic,
-  MonitorSmartphone,
-  Trash2,
-} from "lucide-react";
+import { Import, Mic, MonitorSmartphone, Trash2 } from "lucide-react";
 import { FaGoogleDrive } from "react-icons/fa";
-
-const test_summary: Summary = {
-  id: "0",
-  feedback:
-    "(+) Lorem ipsum dolor sit amet, consectetur adipiscing elit\n(-)Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-  hesitations: 6,
-  fillerWords: 12,
-  transcript:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ",
-};
-
-const TEST_SESSIONS: Session[] = [
-  {
-    id: "0",
-    name: "Session 1",
-    date: new Date(),
-    files: ["notes.txt", "math.txt", "more notes.txt"],
-    summary: test_summary,
-  },
-  {
-    id: "1",
-    name: "Session 2",
-    date: new Date(),
-    files: ["math.txt", "more notes.txt"],
-    summary: test_summary,
-  },
-  {
-    id: "2",
-    name: "Session 3",
-    date: new Date(),
-    files: ["notes.txt", "math.txt", "notes++.txt"],
-    summary: test_summary,
-  },
-  {
-    id: "3",
-    name: "Session 3",
-    date: new Date(),
-    files: ["notes.txt", "math.txt", "notes++.txt"],
-    summary: test_summary,
-  },
-];
 
 const TEST_FILES = ["notes.txt", "math.txt", "notes++.txt"];
 
@@ -86,43 +22,7 @@ export default function TopicDetailedPage({
   params: Promise<{ topicId: string }>;
 }) {
   const { topicId } = use(params);
-  const [viewType, setViewType] = useState(ViewType.Grid);
-
-  // TODO: upload files
-  // const [files, setFiles] = useState(TEST_FILES);
   const files = TEST_FILES;
-
-  // search feature
-  const defaultSessions = TEST_SESSIONS;
-  const [sessionShown, setSessionShown] = useState(defaultSessions);
-  const [search, setSearch] = useState("");
-
-  const searchParams = useSearchParams();
-  // see if isdetail is passed in as a query
-  const details = searchParams.get("details");
-  // TODO: get title
-  // const topicTitle = searchParams.get("title");
-  const topicTitle = "Data Science";
-
-  // to set if the summary tab is showing the detail or list
-  const [isDetail, setIsDetail] = useState(details ? Boolean(details) : false);
-  const [cardClicked, setCardClicked] = useState(0);
-
-  useEffect(() => {
-    if (search) {
-      setSessionShown(
-        defaultSessions.filter((sess) =>
-          sess.name.toLowerCase().includes(search.toLowerCase()),
-        ),
-      );
-    } else {
-      setSessionShown(defaultSessions);
-    }
-  }, [search, defaultSessions]);
-
-  const switchView = () => {
-    setViewType(viewType == ViewType.Grid ? ViewType.List : ViewType.Grid);
-  };
 
   // TODO: create new session ID
   const createNewSession = () => {
@@ -130,124 +30,54 @@ export default function TopicDetailedPage({
   };
 
   return (
-    <Suspense>
-      <div className="px-20 py-10 space-y-8">
-        <h1>{topicTitle}</h1>
-        <Tabs defaultValue={details ? "summary" : "session"}>
-          <TabsList>
-            <TabsTrigger
-              value="session"
-              className="dark:data-[state=active]:text-white"
-            >
-              New Session
-            </TabsTrigger>
-            <TabsTrigger
-              value="summary"
-              className="dark:data-[state=active]:text-white"
-            >
-              Summary
-            </TabsTrigger>
-          </TabsList>
-          <TabsContent value="session">
-            <div className="space-y-8">
-              <h1 className="text-center my-8">Start Recording</h1>
-              {/* recording button */}
-              <Link
-                href={{
-                  pathname: `/t/${topicId}/session`,
-                  query: {
-                    sessionId: createNewSession(),
-                    topic: topicTitle,
-                  },
-                }}
-                className="flex"
-              >
-                <div className="bg-white rounded-full p-10 w-fit justify-center items-center mx-auto inline-block">
-                  <Mic color="#0F172A" size={50} strokeWidth={2} />
-                </div>
-              </Link>
-              {/* list of files */}
-              <h2>Files</h2>
-              <div>
-                {files.map((ele, i) => (
-                  <div key={i} className="flex justify-between space-y-3">
-                    <FileItem fileName={ele} />
-                    <Button variant="secondary" size="sm">
-                      <Trash2 color="#FF383C" />
-                    </Button>
-                  </div>
-                ))}
-              </div>
-              {/* import button */}
-              <DropdownMenu>
-                <DropdownMenuTrigger>
-                  <div className={buttonVariants({ variant: "default" })}>
-                    <Import />
-                    Import
-                  </div>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                  <DropdownMenuItem>
-                    <FaGoogleDrive />
-                    Google Drive
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <MonitorSmartphone />
-                    My Device
-                  </DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-          </TabsContent>
-          <TabsContent value="summary">
-            <div className="my-5">
-              {/* top bar */}
-              {isDetail ? (
-                // return button
-                <Button onClick={() => setIsDetail(false)}>
-                  <ArrowLeft />
-                  Back
-                </Button>
-              ) : (
-                <div className="flex justify-between">
-                  <Button
-                    className="px-0"
-                    variant={"ghost"}
-                    onClick={switchView}
-                  >
-                    {viewType == ViewType.Grid ? (
-                      <>
-                        <LayoutList />
-                        <p>List View</p>
-                      </>
-                    ) : (
-                      <>
-                        <LayoutGrid />
-                        <p>Grid View</p>
-                      </>
-                    )}
-                  </Button>
-                  <SearchBar search={search} setSearch={setSearch} />
-                </div>
-              )}
-              {isDetail ? (
-                <SummaryDetails session={sessionShown[cardClicked]} />
-              ) : (
-                // display sessions list
-                <div
-                  className={`py-5 grid ${viewType == ViewType.Grid ? "grid-cols-3 gap-10" : "grid-cols-1 gap-8"}`}
-                >
-                  {sessionShown.map((item, i) => (
-                    <span key={i} onClick={() => setCardClicked(i)}>
-                      <SessionCard session={item} setIsDetail={setIsDetail} />
-                    </span>
-                  ))}
-                </div>
-              )}
-            </div>
-          </TabsContent>
-        </Tabs>
+    <div className="space-y-8">
+      <h1 className="text-center my-8">Start Recording</h1>
+      {/* recording button */}
+      <Link
+        href={{
+          pathname: `/t/${topicId}/session`,
+          query: {
+            sessionId: createNewSession(),
+            topic: "Data Science", // TODO: get from props or context
+          },
+        }}
+        className="flex"
+      >
+        <div className="bg-white rounded-full p-10 w-fit justify-center items-center mx-auto inline-block">
+          <Mic color="#0F172A" size={50} strokeWidth={2} />
+        </div>
+      </Link>
+      {/* list of files */}
+      <h2>Files</h2>
+      <div>
+        {files.map((ele, i) => (
+          <div key={i} className="flex justify-between space-y-3">
+            <FileItem fileName={ele} />
+            <Button variant="secondary" size="sm">
+              <Trash2 color="#FF383C" />
+            </Button>
+          </div>
+        ))}
       </div>
-    </Suspense>
+      {/* import button */}
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <div className={buttonVariants({ variant: "default" })}>
+            <Import />
+            Import
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem>
+            <FaGoogleDrive />
+            Google Drive
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <MonitorSmartphone />
+            My Device
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
   );
 }
