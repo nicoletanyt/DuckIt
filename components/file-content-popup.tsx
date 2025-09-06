9325e4744383745992001e22c4397a9789108716
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2025-09-06 21:30:46 +0800

    changed styles abit

--- components/file-content-popup.tsx
+++ components/file-content-popup.tsx
@@ -2,6 +2,7 @@ import { TextFile } from "@/lib/TextFile";
 import { X } from "lucide-react";
 import { Dispatch, SetStateAction, useEffect, useRef } from "react";
 import { Button } from "./ui/button";
+import { Separator } from "@/components/ui/separator";
 
 export default function FileContentPopup({
   file,
@@ -11,16 +12,20 @@ export default function FileContentPopup({
   setFileShown: Dispatch<SetStateAction<number>>;
 }) {
   return (
-    <div className="fixed top-[10%] left-[15%] h-[70%] w-[70%] z-50 border-white border-2 bg-black rounded p-10 space-y-10 flex flex-col">
-      <div className="flex justify-between">
-        <h3 className="text-3xl">{file.name}</h3>
-        <Button onClick={() => setFileShown(-1)}>
-          <X></X>
-        </Button>
+    <>
+      <div className="fixed top-[8%] left-[15%] h-[75%] w-[70%] z-50 rounded-sm bg-input py-16 px-24 space-y-14 flex flex-col">
+        <div className="flex justify-between">
+          <div>
+            <h3 className="text-4xl font-semibold">{file.name}</h3>
+          </div>
+          <Button onClick={() => setFileShown(-1)}>
+            <X></X>
+          </Button>
+        </div>
+        <div className="!overflow-y-scroll flex-1 whitespace-pre-line no-scrollbar">
+          <p className="leading-loose">{file.content}</p>
+        </div>
       </div>
-      <div className="!overflow-y-scroll flex-1 whitespace-pre-line">
-        <p className="">{file.content}</p>
-      </div>
-    </div>
+    </>
   );
 }
